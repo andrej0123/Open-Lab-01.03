07366753e00ff3948a0336588d2d80690768ee2b
Andrej Svrček <nepoviem251@gmail.com>
2021-09-29 08:24:42 +0200

    pricitanie + 1

--- Open-Lab-01.03/Exercise.cs
+++ Open-Lab-01.03/Exercise.cs
@@ -6,7 +6,8 @@ namespace Open_Lab_01._03
     {
         public int Addition(int number)
         {
-            throw new NotImplementedException();
+           int c = number + 1;
+            return c;
         }
     }
 }
